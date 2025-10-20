e18c60f47567ccbc7b4a4d4591d39e9700f12355
Tourad <tourad959@gmail.com>
2025-10-20 18:37:31 +0000

    Initial commit

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -5,6 +5,7 @@ from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
+from langchain_huggingface import HuggingFaceEmbeddings
 
 # =============================
 # 1. Télécharger la base vectorielle depuis Hugging Face
